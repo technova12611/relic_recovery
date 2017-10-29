76b182eccc29ab14c4933f7c4bb65e814e115444
TechNova12611 <ftcteam2611@gmail.com>
2017-10-29 08:46:25 -0700

    Change slow mode button mapping, no test yet!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechNova2017/DriveHelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechNova2017/DriveHelper.java
@@ -50,7 +50,7 @@ public class DriveHelper {
                 scaleToDrive = 1.0;
             }
         }
-        else if(g.XOnce() || (g.leftBumper() && g.rightBumper())) {
+        else if(g.XOnce() || (g.leftBumperOnce() && g.rightBumperOnce())) {
             slowDrive = !slowDrive;
             if(slowDrive) {
                 scaleToDrive = LOW_MODE_SCALE_TO_DRIVE;
